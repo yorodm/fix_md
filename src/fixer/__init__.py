3bc421b6b877827c50d2f8445fb2d262508590b0
Yoandy Rodriguez Martinez <yoandy.rodriguez@musala.com>
2024-02-20 13:25:56 +0200

    fix: add stubs for methods

--- src/fixer/__init__.py
+++ src/fixer/__init__.py
@@ -8,7 +8,7 @@ parser and writing a simple OrgMode renderer
 import frontmatter
 import click
 from mistletoe.base_renderer import BaseRenderer
-from mistletoe import Document
+from mistletoe import Document, block_token, span_token
 from pathlib import Path
 
 
@@ -19,6 +19,89 @@ class OrgRenderer(BaseRenderer):
         """Initialize the renderer."""
         self.metadata = metadata
         super().__init__(extras, kwargs)
+
+    def render_raw_text(self, token) -> str:
+        """
+        Default render method for RawText. Simply return token.content.
+        """
+        return token.content
+
+    def render_strong(self, token: span_token.Strong) -> str:
+        """Render bold."""
+        return self.render_inner(token)
+
+    def render_emphasis(self, token: span_token.Emphasis) -> str:
+        """Render emph."""
+        return self.render_inner(token)
+
+    def render_inline_code(self, token: span_token.InlineCode) -> str:
+        """Render inline code."""
+        return self.render_inner(token)
+
+    def render_strikethrough(self, token: span_token.Strikethrough) -> str:
+        """Render strikethrough."""
+        return self.render_inner(token)
+
+    def render_image(self, token: span_token.Image) -> str:
+        """Render image link."""
+        return self.render_inner(token)
+
+    def render_link(self, token: span_token.Link) -> str:
+        """Render link."""
+        return self.render_inner(token)
+
+    def render_auto_link(self, token: span_token.AutoLink) -> str:
+        """Render link."""
+        return self.render_inner(token)
+
+    def render_escape_sequence(self, token: span_token.EscapeSequence) -> str:
+        """Render scape sequence."""
+        return self.render_inner(token)
+
+    def render_line_break(self, token: span_token.LineBreak) -> str:
+        """Render line break."""
+        return self.render_inner(token)
+
+    def render_heading(self, token: block_token.Heading) -> str:
+        """Render headings."""
+        return self.render_inner(token)
+
+    def render_quote(self, token: block_token.Quote) -> str:
+        """Render quotes."""
+        return self.render_inner(token)
+
+    def render_paragraph(self, token: block_token.Paragraph) -> str:
+        """Render paragram."""
+        return self.render_inner(token)
+
+    def render_block_code(self, token: block_token.BlockCode) -> str:
+        """Render code block."""
+        return self.render_inner(token)
+
+    def render_list(self, token: block_token.List) -> str:
+        """Render list."""
+        return self.render_inner(token)
+
+    def render_list_item(self, token: block_token.ListItem) -> str:
+        """Render list item."""
+        return self.render_inner(token)
+
+    def render_table(self, token: block_token.Table) -> str:
+        """Render table."""
+        return self.render_inner(token)
+
+    def render_table_cell(self, token: block_token.TableCell) -> str:
+        """Render table cell."""
+        return self.render_inner(token)
+
+    def render_table_row(self, token: block_token.TableRow) -> str:
+        """Render table row"""
+        return self.render_inner(token)
+
+    def render_thematic_break(self, token: block_token.ThematicBreak) -> str:
+        """Render whatever this is."""
+        return self.render_inner(token)
+
 
 
 class Runner:
